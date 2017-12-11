6e5feb05a32525873bac233b6890c835c5ea2315
Jaqueline Kimmi Kirino <kimmi.kirino@prontmed.com>
2017-12-11 16:17:23 -0200

    Leap test

---
+++ leap/leap.js
@@ -0,0 +1,10 @@
+class Year {
+  constructor(year) {
+    this.year = year;
+  }
+  isLeap() {
+    return this.year%4 === 0 && !(this.year%100 === 0  && this.year%400 !== 0);
+  }
+}
+
+export default Year;
